e9a385cb256f3c1ffcdee30d6ddf4dfbc589e7b9
Rafal Maj <rmaj91@gmail.com>
2020-12-15 18:49:54 +0100

    init

---
+++ src/main/java/com/the/dark/side/crew/fejsbuk/FejsbukApplication.java
@@ -0,0 +1,13 @@
+package com.the.dark.side.crew.fejsbuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FejsbukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FejsbukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/the/dark/side/crew/fejsbuk/FejsbukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the.dark.side.crew.fejsbuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FejsbukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
