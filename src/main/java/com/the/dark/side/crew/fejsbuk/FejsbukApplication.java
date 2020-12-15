5ee3c112fd9229ebff1d11971b02eb5f76029726
Rafal Maj <rmaj91@gmail.com>
2020-12-15 19:11:36 +0100

    [JABERA-4] moj commit

--- src/main/java/com/the/dark/side/crew/fejsbuk/FejsbukApplication.java
+++ src/main/java/com/the/dark/side/crew/fejsbuk/FejsbukApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FejsbukApplication {
-
+awdawd
 	public static void main(String[] args) {
 		SpringApplication.run(FejsbukApplication.class, args);
 	}
