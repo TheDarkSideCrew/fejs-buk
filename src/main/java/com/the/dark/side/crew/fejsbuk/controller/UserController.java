96110db8001ec3a57a52da19a3838f2a0405cbc4
jkk <jarekkloc84@gmail.com>
2020-12-19 16:36:53 +0100

    JABERA-4 added GetUser and some small fixes

--- src/main/java/com/the/dark/side/crew/fejsbuk/controller/UserController.java
+++ src/main/java/com/the/dark/side/crew/fejsbuk/controller/UserController.java
@@ -14,12 +14,20 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    // get users list
     @GetMapping("users")
     public List<User> getUsers() {
         return userService.getUsers();
     }
 
-    @PostMapping("register")
+    // get a specific user details
+    @GetMapping("users/{id}")
+    public User getUser(@PathVariable long id) {
+        return userService.getUser(id);
+    }
+
+    // register new user
+    @PostMapping("users/new")
     public User addUser(@RequestBody User user) {
         return userService.addUser(user);
     }
