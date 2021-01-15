76673d087a94ac10f609fb0d483e4d4255024909
jkk <jarekkloc84@gmail.com>
2021-01-15 18:37:05 +0100

    JABERA-4 change to ResponseEntity in UserController

--- src/main/java/com/the/dark/side/crew/fejsbuk/controller/UserController.java
+++ src/main/java/com/the/dark/side/crew/fejsbuk/controller/UserController.java
@@ -4,24 +4,25 @@ import com.the.dark.side.crew.fejsbuk.model.User;
 import com.the.dark.side.crew.fejsbuk.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
 public class UserController {
 
     @Autowired
     private final UserService userService;
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable long id) {
-        return userService.getUser(id);
+    public ResponseEntity<User> getUser(@PathVariable long id) {
+        return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
     }
 
     @PostMapping
-    public User addUser(@RequestBody User user) {
-        return userService.addUser(user);
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.addUser(user), HttpStatus.OK);
     }
 }
