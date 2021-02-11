d2ae0ee221b8af7e420f71064ff7288a00b6ff29
jkk <jarekkloc84@gmail.com>
2021-02-11 21:07:09 +0100

    JABERA-5 add tests

---
+++ src/test/java/com/the/dark/side/crew/fejsbuk/mapper/PostMapperTest.java
@@ -0,0 +1,99 @@
+package com.the.dark.side.crew.fejsbuk.mapper;
+
+import com.the.dark.side.crew.fejsbuk.model.PostEntity;
+import com.the.dark.side.crew.fejsbuk.model.UserEntity;
+import com.the.dark.side.crew.fejsbuk.model.dto.PostDto;
+import com.the.dark.side.crew.fejsbuk.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+class PostMapperTest {
+
+    @InjectMocks
+    private PostMapper postMapper;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void whenCallToDtosThenReturnListOfDtos() {
+        PostMapper mapper = spy(new PostMapper(userRepository));
+        PostDto postDto = mock(PostDto.class);
+        PostEntity postEntity = mock(PostEntity.class);
+
+        doReturn(postDto).when(mapper).toDto(postEntity);
+
+        List<PostDto> result = mapper.toDtos(List.of(postEntity));
+        assertEquals(1, result.size());
+        assertEquals(postDto, result.get(0));
+    }
+
+    @Test
+    void whenCallToDtoThenReturnPostDto() {
+        PostEntity postEntity = mock(PostEntity.class);
+        UserEntity user = mock(UserEntity.class);
+        LocalDateTime createdAt = LocalDateTime.now();
+        long postId = 1L;
+        long userId = 2L;
+        String message = "message";
+
+        when(postEntity.getId()).thenReturn(postId);
+        when(postEntity.getMessage()).thenReturn(message);
+        when(postEntity.getCreatedAt()).thenReturn(createdAt);
+        when(postEntity.getUserEntity()).thenReturn(user);
+        when(user.getId()).thenReturn(userId);
+
+        PostDto result = postMapper.toDto(postEntity);
+        assertEquals(postId, result.getId());
+        assertEquals(message, result.getMessage());
+        assertEquals(createdAt, result.getCreatedAt());
+        assertEquals(userId, result.getUserId());
+    }
+
+    @Test
+    void whenUserFoundThenReturnPost() {
+        long userId = 2L;
+        String message = "message";
+        UserEntity userEntity = mock(UserEntity.class);
+        PostDto postDto = mock(PostDto.class);
+
+        when(postDto.getMessage()).thenReturn(message);
+        when(postDto.getUserId()).thenReturn(userId);
+        when(userRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(userEntity.getId()).thenReturn(userId);
+
+        PostEntity result = postMapper.toEntity(postDto);
+        assertEquals(message, result.getMessage());
+        assertEquals(userId, result.getUserEntity().getId());
+    }
+
+    @Test
+    void whenUserNotFoundThenReturn404() {
+        long userId = 2L;
+        PostDto postDto = mock(PostDto.class);
+
+        when(postDto.getUserId()).thenReturn(userId);
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        ResponseStatusException exception =
+                assertThrows(ResponseStatusException.class, () -> postMapper.toEntity(postDto));
+        assertEquals("404 NOT_FOUND \"User 2 not found.\"", exception.getMessage());
+    }
+}
--- src/test/java/com/the/dark/side/crew/fejsbuk/service/impl/PostServiceTest.java
+++ src/test/java/com/the/dark/side/crew/fejsbuk/service/impl/PostServiceTest.java
@@ -2,72 +2,67 @@ package com.the.dark.side.crew.fejsbuk.service.impl;
 
 import com.the.dark.side.crew.fejsbuk.mapper.PostMapper;
 import com.the.dark.side.crew.fejsbuk.model.PostEntity;
-import com.the.dark.side.crew.fejsbuk.model.UserEntity;
 import com.the.dark.side.crew.fejsbuk.model.dto.PostDto;
 import com.the.dark.side.crew.fejsbuk.repository.PostRepository;
-import com.the.dark.side.crew.fejsbuk.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class PostServiceTest {
 
     @InjectMocks
     private PostServiceImpl postService;
 
-    @InjectMocks
-    private PostMapper postMapper;
-
-    @Spy
+    @Mock
     private PostRepository postRepository;
 
-    @Spy
-    private UserRepository userRepository;
+    @Mock
+    private PostMapper postMapper;
 
     @BeforeEach
     void init() {
         MockitoAnnotations.initMocks(this);
     }
 
-//    @Test
-//    void shouldReturnAllPosts() {
-//        // given
-//        long userId = 1L;
-//        PostDto postDto = mock(PostDto.class);
-//        postDto.setUserId(userId);
-//
-////        when(postRepository.findByUserEntityId(userId)).thenReturn(List.of(postEntity));
-//        when(postMapper.toDtos(postRepository.findByUserEntityId(userId))).thenReturn(List.of(postDto));
-//
-//        List<PostDto> result = postService.getAllPosts(userId);
-//        assertTrue(result.);
-//        assertArrayEquals(result, postDto);
-//    }
+    @Test
+    void whenUserFoundThanReturnAllPosts() {
+        long userId = 1L;
+        PostDto postDto = mock(PostDto.class);
+
+        when(postMapper.toDtos(postRepository.findByUserEntityId(userId))).thenReturn(List.of(postDto));
+
+        List<PostDto> result = postService.getAllPosts(userId);
+        assertEquals(1, result.size());
+        assertEquals(postDto, result.get(0));
+    }
+
+    @Test
+    void whenUserNotFoundThanReturnEmptyList() {
+        long userId = 1L;
+
+        when(postMapper.toDtos(postRepository.findByUserEntityId(userId))).thenReturn(List.of());
+
+        List<PostDto> result = postService.getAllPosts(userId);
+        assertEquals(0, result.size());
+    }
 
     @Test
     void shouldAddPost() {
-        // given
-        PostDto postDto = spy(PostDto.class);
-        PostEntity postEntity = spy(PostEntity.class);
-        UserEntity userEntity = spy(UserEntity.class);
-        userEntity.setId(1);
-        postDto.setUserId(1);
-        postEntity.setId(1);
-        postEntity.setUserEntity(userEntity);
+        PostDto postDto = mock(PostDto.class);
+        PostEntity postEntity = mock(PostEntity.class);
 
-        // when
-        when(postRepository.save(postMapper.toEntity(postDto)))
-                .thenReturn(postEntity);
+        when(postMapper.toEntity(postDto)).thenReturn(postEntity);
+        when(postRepository.save(postEntity)).thenReturn(postEntity);
+        when(postMapper.toDto(postEntity)).thenReturn(postDto);
 
-        // then
-//        PostDto post = postMapper.toDto(postService.addPost(postDto));
-//        post.setUserId(1);
-//        verify(postRepository).save(postMapper.toEntity(postDto));
-//        assertEquals(post, postDto);
+        PostDto result = postService.addPost(postDto);
+        assertEquals(result, postDto);
     }
 }
