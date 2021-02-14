35f7ab4a75ec58f1f7327a3e93907ea048d19330
jkk <jarekkloc84@gmail.com>
2021-02-14 15:52:15 +0100

    JABERA-6 add tests

---
+++ src/test/java/com/the/dark/side/crew/fejsbuk/mapper/CommentMapperTest.java
@@ -0,0 +1,133 @@
+package com.the.dark.side.crew.fejsbuk.mapper;
+
+import com.the.dark.side.crew.fejsbuk.model.CommentEntity;
+import com.the.dark.side.crew.fejsbuk.model.PostEntity;
+import com.the.dark.side.crew.fejsbuk.model.UserEntity;
+import com.the.dark.side.crew.fejsbuk.model.dto.CommentDto;
+import com.the.dark.side.crew.fejsbuk.repository.PostRepository;
+import com.the.dark.side.crew.fejsbuk.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+class CommentMapperTest {
+
+    @InjectMocks
+    private CommentMapper commentMapper;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PostRepository postRepository;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void whenCallToDtosThenReturnListOfDtos() {
+        CommentMapper mapper = spy(new CommentMapper(userRepository, postRepository));
+        CommentDto commentDto = mock(CommentDto.class);
+        CommentEntity commentEntity = mock(CommentEntity.class);
+
+        doReturn(commentDto).when(mapper).toDto(commentEntity);
+
+        List<CommentDto> result = mapper.toDtos(List.of(commentEntity));
+        assertEquals(1, result.size());
+        assertEquals(commentDto, result.get(0));
+    }
+
+    @Test
+    void whenCallToDtoThenReturnPostDto() {
+        CommentEntity commentEntity = mock(CommentEntity.class);
+        PostEntity postEntity = mock(PostEntity.class);
+        UserEntity userEntity = mock(UserEntity.class);
+        LocalDateTime createdAt = LocalDateTime.now();
+        long commentId = 3L;
+        long postId = 1L;
+        long userId = 2L;
+        String message = "message";
+
+        when(commentEntity.getId()).thenReturn(commentId);
+        when(commentEntity.getMessage()).thenReturn(message);
+        when(commentEntity.getCreatedAt()).thenReturn(createdAt);
+        when(commentEntity.getUserEntity()).thenReturn(userEntity);
+        when(commentEntity.getPostEntity()).thenReturn(postEntity);
+        when(userEntity.getId()).thenReturn(userId);
+        when(postEntity.getId()).thenReturn(postId);
+
+        CommentDto result = commentMapper.toDto(commentEntity);
+        assertEquals(commentId, result.getId());
+        assertEquals(message, result.getMessage());
+        assertEquals(createdAt, result.getCreatedAt());
+        assertEquals(postId, result.getPostId());
+        assertEquals(userId, result.getUserId());
+    }
+
+    @Test
+    void whenUserAndPostFoundThenReturnComment() {
+        long userId = 1L;
+        long postId = 2L;
+        String message = "message";
+        UserEntity userEntity = mock(UserEntity.class);
+        PostEntity postEntity = mock(PostEntity.class);
+        CommentDto commentDto = mock(CommentDto.class);
+
+        when(commentDto.getMessage()).thenReturn(message);
+        when(commentDto.getUserId()).thenReturn(userId);
+        when(commentDto.getPostId()).thenReturn(postId);
+        when(userRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(postRepository.findById(postId)).thenReturn(Optional.of(postEntity));
+        when(userEntity.getId()).thenReturn(userId);
+        when(postEntity.getId()).thenReturn(postId);
+
+        CommentEntity result = commentMapper.toEntity(commentDto);
+        assertEquals(message, result.getMessage());
+        assertEquals(userId, result.getUserEntity().getId());
+        assertEquals(postId, result.getPostEntity().getId());
+    }
+
+    @Test
+    void whenUserNotFoundThenReturn404() {
+        long userId = 2L;
+        CommentDto commentDto = mock(CommentDto.class);
+
+        when(commentDto.getUserId()).thenReturn(userId);
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        ResponseStatusException exception =
+                assertThrows(ResponseStatusException.class, () -> commentMapper.toEntity(commentDto));
+        assertEquals("404 NOT_FOUND \"User 2 not found.\"", exception.getMessage());
+    }
+
+    @Test
+    void whenPostNotFoundThenReturn404() {
+        long postId = 2L;
+        long userId = 2L;
+        UserEntity userEntity = mock(UserEntity.class);
+        CommentDto commentDto = mock(CommentDto.class);
+
+        when(commentDto.getPostId()).thenReturn(postId);
+        when(commentDto.getUserId()).thenReturn(userId);
+        when(userRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(postRepository.findById(postId)).thenReturn(Optional.empty());
+        when(userEntity.getId()).thenReturn(userId);
+
+        ResponseStatusException exception =
+                assertThrows(ResponseStatusException.class, () -> commentMapper.toEntity(commentDto));
+        assertEquals("404 NOT_FOUND \"Post 2 not found.\"", exception.getMessage());
+    }
+}
---
+++ src/test/java/com/the/dark/side/crew/fejsbuk/service/impl/CommentServiceImplTest.java
@@ -0,0 +1,80 @@
+package com.the.dark.side.crew.fejsbuk.service.impl;
+
+import com.the.dark.side.crew.fejsbuk.mapper.CommentMapper;
+import com.the.dark.side.crew.fejsbuk.model.CommentEntity;
+import com.the.dark.side.crew.fejsbuk.model.PostEntity;
+import com.the.dark.side.crew.fejsbuk.model.dto.CommentDto;
+import com.the.dark.side.crew.fejsbuk.repository.CommentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+class CommentServiceImplTest {
+
+    @InjectMocks
+    private CommentServiceImpl commentService;
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @Mock
+    private CommentMapper commentMapper;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void whenPostFoundThenReturnAllComments() {
+        long postId = 2L;
+        CommentDto commentDto = mock(CommentDto.class);
+        CommentEntity commentEntity = mock(CommentEntity.class);
+        PostEntity postEntity = mock(PostEntity.class);
+
+        when(commentRepository.findByPostEntityId(postId))
+                .thenReturn(List.of(commentEntity));
+        when(commentMapper.toDtos(List.of(commentEntity)))
+                .thenReturn(Collections.singletonList(commentDto));
+        when(postEntity.getId()).thenReturn(postId);
+
+        List<CommentDto> result = commentService.getAllPostIdComments(postId);
+        assertEquals(1, result.size());
+        assertEquals(commentDto, result.get(0));
+    }
+
+    @Test
+    void whenPostNotFoundThenReturnEmptyList() {
+        long postId = 2L;
+
+        when(commentRepository.findByPostEntityId(postId))
+                .thenReturn(Collections.emptyList());
+        when(commentMapper.toDtos(Collections.emptyList()))
+                .thenReturn(Collections.emptyList());
+
+        List<CommentDto> result = commentService.getAllPostIdComments(postId);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldAddComment() {
+        CommentDto commentDto = mock(CommentDto.class);
+        CommentEntity commentEntity = mock(CommentEntity.class);
+
+        when(commentMapper.toEntity(commentDto)).thenReturn(commentEntity);
+        when(commentMapper.toDto(commentEntity)).thenReturn(commentDto);
+
+        CommentDto result = commentService.addComment(commentDto);
+        verify(commentRepository).save(commentEntity);
+        assertEquals(result, commentDto);
+    }
+}
